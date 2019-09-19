af1e01d0ceee8ff7de1f9a890086fcbd93a69446
soap27century <soap27century@163.com>
2019-09-19 06:21:59 +0200

    asd

--- chengsi_2_client.py
+++ chengsi_2_client.py
@@ -1,11 +1,11 @@
 import asyncio
 
 class EchoClient(asyncio.Protocol):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def connection_made(self, transport):
-		self.transport = transport
+    def connection_made(self, transport):
+        self.transport = transport
 
     def data_received(self, data):
         print(data)
@@ -37,14 +37,16 @@ class EchoClient(asyncio.Protocol):
                         print('h')
 
 if __name__ == "__main__":
-	loop = asyncio.get_event_loop()
-	coro = loop.create_connection(EchoClient,'192.168.200.52',19003)
-	client = loop.run_until_complete(coro)
+        loop = asyncio.get_event_loop()
+        coro = loop.create_connection(EchoClient,'192.168.200.52',19003)
+        client = loop.run_until_complete(coro)
 
-	try:
-		loop.run_forever()
-	except KeyboardInterrupt:
-		pass
-		client.close()
-		loop.run_until_complete(client.close())
-	loop.close()
+        try:
+                loop.run_forever()
+        except KeyboardInterrupt:
+                pass
+        client.close()
+        loop.run_until_complete(client.close())
+        loop.close()
+
+
